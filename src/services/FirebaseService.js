cc12d2a083d664882395edb1e83e6ff5d18eabd4
유승주 <seung338989@gmail.com>
2019-07-10 15:31:52 +0900

    190710 | S1P2211021-30 completed

--- src/services/FirebaseService.js
+++ src/services/FirebaseService.js
@@ -72,28 +72,27 @@ export default {
 		})
 	},
 
-
-	// getPortfolios() {
-	// 	const postsCollection = firestore.collection(PORTFOLIOS)
-	// 	return postsCollection
-	// 			.orderBy('created_at', 'desc')
-	// 			.get()
-	// 			.then((docSnapshots) => {
-	// 				return docSnapshots.docs.map((doc) => {
-	// 					let data = doc.data()
-	// 					data.created_at = new Date(data.created_at.toDate())
-	// 					return data
-	// 				})
-	// 			})
-	// },
-	// postPortfolio(title, body, img) {
-	// 	return firestore.collection(PORTFOLIOS).add({
-	// 		title,
-	// 		body,
-	// 		img,
-	// 		created_at: firebase.firestore.FieldValue.serverTimestamp()
-	// 	})
-	// },
+	getPortfolios() {
+		const postsCollection = firestore.collection("Portfolios")
+		return postsCollection
+				.orderBy('created_at', 'desc')
+				.get()
+				.then((docSnapshots) => {
+					return docSnapshots.docs.map((doc) => {
+						let data = doc.data()
+						data.created_at = new Date(data.created_at.toDate())
+						return data
+					})
+				})
+	},
+	postPortfolio(title, body, img) {
+		return firestore.collection("Portfolios").add({
+			title,
+			body,
+			img,
+			created_at: firebase.firestore.FieldValue.serverTimestamp()
+		})
+	},
 	// loginWithGoogle() {
 	// 	let provider = new firebase.auth.GoogleAuthProvider()
 	// 	return firebase.auth().signInWithPopup(provider).then(function(result) {
