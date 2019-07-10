5b199b5faa255e4dbf91965bdaedeff94518fc5b
임현철 <lhc6005@hanmail.net>
2019-07-10 09:30:44 +0900

    190710 S1P2212005-35 header 영역에 블로그 제목 기제 및, 폰트와 margin 적용

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="header">
-    <div class="center">
-    <h1>헤더입니다.</h1>
+    <div style="margin-left:2%;">
+      <h2 style="font-family: 'Gugi', cursive;">SSAFY 원투쓰리!</h2>
     </div>
 
 
@@ -18,3 +18,7 @@ export default ({
 })
 
 </script>
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Black+Han+Sans|Gugi&display=swap');
+</style>
